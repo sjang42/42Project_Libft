a174b11d535591414bd115bd9038f66213a7ad16
Sanghyun Jang <jkkjkkr@naver.com>
2016-11-27 16:12:49 -0800

    fix ft_realloc.c

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: sjang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/26 02:08:15 by sjang             #+#    #+#             */
-/*   Updated: 2016/11/26 02:08:23 by sjang            ###   ########.fr       */
+/*   Updated: 2016/11/27 16:12:25 by sjang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void			*ft_realloc(void *some, size_t size_cur, size_t size_fut)
 {
 	void *new;
 
-	new = malloc(sizeof(size_fut));
+	new = malloc(size_fut);
 	if (size_cur <= size_fut)
 		ft_memcpy(new, some, size_cur);
 	else
